c65d543753e15066d325d8908cb4e6e6b534d727
Sam Walls <sam@feischemonger.com>
2016-09-24 03:47:22 +0100

    main now uses iterator for getChildren as example

--- main.cpp
+++ main.cpp
@@ -7,11 +7,10 @@
  */
 int main() {
     NTree<int, 3>* tree = new NTree<int, 3>(5);
-    tree->getRoot()->addChild(1);
-    tree->getRoot()->addChild(2);
-    tree->getRoot()->addChild(3);
-    for (int i = 0; i < 3; i++) {
-        Node<int, 3>* child = tree->getRoot()->child(i);
+    tree->getRoot()->setChild(0, 1);
+    tree->getRoot()->setChild(1, 2);
+    tree->getRoot()->setChild(2, 3);
+    for (Node<int, 3>* child : tree->getRoot()->getChildren()) {
         std::cout << child->toString() << std::endl;
     }
     delete tree;
