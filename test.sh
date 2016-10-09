39013ee34106636d55c36aaaa6ea25a224fac437
Sam Walls <sam@feischemonger.com>
2016-10-09 04:02:19 +0100

    fixed test script again

--- test.sh
+++ test.sh
@@ -3,10 +3,7 @@ N=$1
 for i in `seq 1 $N`;
 do
 	echo "running test sequence ${i}"
-	echo "running tar tests"
-	#unbuffer ./testset.sh tar tar 2>&1 | tee  "logs/run${i}.log"
-	echo "running txt tests"
-	unbuffer ./testset.sh txt txt 2>&1 | tee "logs/run${i}.log"
-	echo "running img tests"
-	#unbuffer ./testset.sh img bmp 2>&1 | tee "logs/run${i}.log"
+	./testset.sh tar tar 2>&1 | tee  --append "logs/run${i}.log"
+	./testset.sh txt txt 2>&1 | tee --append "logs/run${i}.log"
+	./testset.sh img bmp 2>&1 | tee --append "logs/run${i}.log"
 done
